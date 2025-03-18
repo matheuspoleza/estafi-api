32b1431a9ae833b5cac102d9c4bded998fabaa1f
Matheus Poleza <matheus.poleza@voiceflow.com>
2025-03-18 15:19:10 -0400

    fix: fixes port

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,8 @@ import { BullModule } from '@nestjs/bull';
   imports: [
     BullModule.forRoot({
       redis: {
-        host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_PORT),
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT) || 6379,
       },
     }),
     WhatsappModule,
